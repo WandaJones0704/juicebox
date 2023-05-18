d6ee3ec0ed4338d6a30baf2af8a670c4468e6c89
Wanda Jones <wandam.jones0704@yahoo.com>
2023-05-18 11:16:25 -0400

    partway through 2.2

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+require("dotenv").config();
 const PORT = 3000;
-const express = require('express');
+const express = require("express");
 const server = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const{client} = require('./db');
+const { client } = require("./db");
 client.connect();
 
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use(express.json());
 
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
 
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
 
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
